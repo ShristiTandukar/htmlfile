e798365644ec4f6cdbf527750e9806bb14405ef5
ShristiTandukar <98247895+ShristiTandukar@users.noreply.github.com>
2023-05-02 15:16:37 +0545

    Add files via upload

---
+++ index.php
@@ -0,0 +1,160 @@
+<html>
+    <head>
+        <title>
+            Welcome to my website.
+        </title>
+        <!--bootstrap css link-->
+        <link rel="stylesheet" href= " https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css"/>
+        <!--front awesome link -->
+        <link rel="stylesheet" href= " https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"/>
+        <!--bootstrap js link -->
+       <link rel="stylesheet" href= "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css"/>
+        <!--css style-->
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <div class="container-fluid p-0">
+            <nav class="navbar navbar-expand-lg  navbar-dark bg-success bg-gradient">
+                <div class="container-fluid ">
+                    <img src ="img/logo.jfif " alt="">
+                  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                  </button>
+                  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                      <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="#">Products</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="#">Register</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="#">Contact</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="#"><i class="fa fa-shopping-cart" ></i><sup></sup></a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="#">Total Price : 100/-</a>
+                      </li>
+                     
+                    </ul>
+                    <form class="d-flex">
+                      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                      <button class="btn btn-outline-light" type="submit">Search</button>
+                    </form>
+                  </div>
+                </div>
+              </nav>
+
+              <!--second child-->
+             <nav class="navbar navbar-expand-lg   navbar-dark bg-dark  bg-gradient">
+                 <ul class="navbar-nav me-auto">
+                 <li class="nav-item">
+            <a class="nav-link" href="#">Welcome Guest</a> 
+             </li>
+             <li class="nav-item">
+              <a class="nav-link" href="#">Login</a> 
+             </li>
+            </ul>
+          </nav>
+
+          <!--third child-->
+             <div class=bg-success bg-gradient>
+              <h3 class="text-center">Online Store</h3>
+              <p class="text-center">Life is too short to wear boring clothes.</p>
+             </div>
+
+             <!--fourth child-->
+             <div class="row">
+              <div class="col-md-10">
+                 <!--products-->
+                 <div class="row">
+                 <div class="col-md-4"> 
+                 <div class="card" >
+                  <img src ="img/222.jfif" class="card-img-top" alt="...">
+                  <div class="card-body">
+                    <h5 class="card-title">Woolen Sweater</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn bg-success bg-gradient">Add to cart </a>
+                    <a href="#" class="btn btn-secondary">View More </a>
+                  </div>
+                </div>
+                 </div>
+             
+                <div class="col-md-4"> 
+                    <div class="card" >
+                    <img src ="img/111.png" class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h5 class="card-title">Double Layer Jacket</h5>
+                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                      <a href="#" class="btn bg-success bg-gradient">Add to cart </a>
+                    <a href="#" class="btn btn-secondary">View More </a>
+                    </div>
+                  </div>
+                  </div>
+                  
+                    <div class="col-md-4">
+                      <div class="card" >
+                        <img src="img/333.jfif " class="card-img-top" alt="...">
+                        <div class="card-body">
+                          <h5 class="card-title">Windcheater</h5>
+                          <p class="card-text">Waterproof Windproof Double Layer Windcheater Jacket for men.</p>
+                          <a href="#" class="btn bg-success bg-gradient">Add to cart </a>
+                          <a href="#" class="btn btn-secondary">View More </a>
+                        </div>
+                      </div>
+                      
+                    </div>
+
+                    <div class="col-md-4 mb-2">
+                      <div class="card" >
+                        <img src="img/444.jfif " class="card-img-top" alt="...">
+                        <div class="card-body">
+                          <h5 class="card-title">Printed T-shirt</h5>
+                          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                          <a href="#" class="btn bg-success bg-gradient">Add to cart </a>
+                          <a href="#" class="btn btn-secondary">View More </a>
+                        </div>
+                      </div>
+                      
+                    </div>
+                 
+                    <div class="col-md-4 mb-2">
+                      <div class="card" >
+                        <img src="img/jeans.jfif " class="card-img-top" alt="...">
+                        <div class="card-body">
+                          <h5 class="card-title">Leather Jacket</h5>
+                          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                          <a href="#" class="btn bg-success bg-gradient">Add to cart </a>
+                          <a href="#" class="btn btn-secondary">View More </a>
+                        </div>
+                      </div>
+                      
+                    </div>
+                    <div class="col-md-4 mb-2">
+                      <div class="card" >
+                        <img src="img/555.jfif " class="card-img-top" alt="...">
+                        <div class="card-body">
+                          <h5 class="card-title">Casual Shirt</h5>
+                          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                          <a href="#" class="btn bg-success bg-gradient">Add to cart </a>
+                          <a href="#" class="btn btn-secondary">View More </a>
+                        </div>
+                      </div>
+                      </div>
+                      
+               <div class="col-md-2 mb-2  bg-secondary p-0">
+                <ul class="navbar-nav me-auto text-center"></ul>
+                <li class="nav-item bg-success bg-gradient"></li>
+                <a href="#" class="nav-link text-light">Clothing</a>
+              </div>
+           </div>
+        </div>
+</div>
+        
+    </body>
+    </html>
